e2d4fb0d41a6edd4660b41c6a5c61ee2b19f2830
nazimai <809670160@qq.com>
2020-07-21 16:30:11 +0800

    transform and gradient

--- AnimationTest/GradientViewController.m
+++ AnimationTest/GradientViewController.m
@@ -5,7 +5,11 @@
 //  Created by nazimai on 2020/7/20.
 //  Copyright © 2020 nazimai. All rights reserved.
 //
-
+/**
+ * 关于 CAGradientLayer 这里有一个争论，iPhone 的坐标系到底从哪开始？
+ * 正常情况下，一般左上角是 (0,  0），从下面的 startPoint 和 endPoint 可以得出此结论。
+ * 但是网上也有不同看法，暂时没找到官方文档。
+ */
 #import "GradientViewController.h"
 
 @interface GradientViewController ()
@@ -17,6 +21,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.whiteColor;
+    UIView *contentView = [UIView new];
+    contentView.backgroundColor = [UIColor colorWithRed:240 / 250.0 green:240 / 250.0 blue:240 / 250.0 alpha:0.9];
+    contentView.bounds = CGRectMake(0, 0, 256, 256);
+    contentView.center = self.view.center;
+    [self.view addSubview:contentView];
+
+    CAGradientLayer *layer = [CAGradientLayer layer];
+    layer.frame = contentView.bounds;
+    layer.colors = @[(__bridge id)UIColor.blueColor.CGColor, (__bridge id)UIColor.brownColor.CGColor, (__bridge id)UIColor.systemPinkColor.CGColor];
+    layer.locations = @[@0.0, @0.2, @0.6];
+    layer.startPoint = CGPointMake(0, 0);
+    layer.endPoint = CGPointMake(1, 1);
+    [contentView.layer addSublayer:layer];
 }
 
 /*
--- AnimationTest/TransformViewController.m
+++ AnimationTest/TransformViewController.m
@@ -10,6 +10,8 @@
 
 @interface TransformViewController ()
 
+@property (nonatomic, strong) UIView *contentView;
+
 @end
 
 @implementation TransformViewController
@@ -17,6 +19,81 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.whiteColor;
+    UIView *contentView = [UIView new];
+    contentView.backgroundColor = [UIColor colorWithRed:240 / 250.0 green:240 / 250.0 blue:240 / 250.0 alpha:0.9];
+    contentView.bounds = CGRectMake(0, 0, 256, 256 * 2.5);
+    contentView.center = self.view.center;
+    [self.view addSubview:contentView];
+    self.contentView = contentView;
+
+    // 设置透视变换
+    CATransform3D pt = CATransform3DIdentity;
+    pt.m34 = -1.0 / 500.0;
+    contentView.layer.sublayerTransform = pt;
+
+    // 设置立方体 1
+    CATransform3D c1t = CATransform3DIdentity;
+    c1t = CATransform3DTranslate(c1t, -100, 0, 0);
+    CALayer *cu1 = [self cubeWithTransform:c1t];
+    [self.contentView.layer addSublayer:cu1];
+
+    // 设置立方体 2
+    CATransform3D c2t = CATransform3DIdentity;
+    c2t = CATransform3DTranslate(c2t, 100, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 1, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 0, 1, 0);
+    CALayer *cube2 = [self cubeWithTransform:c2t];
+    [self.contentView.layer addSublayer:cube2];
+}
+
+- (CALayer *)cubeWithTransform:(CATransform3D)transfrom {
+    // 创建立方体 layer
+    CATransformLayer *cubeLayer = [CATransformLayer layer];
+
+    // 添加 面 1 - 6
+    CATransform3D ct = CATransform3DMakeTranslation(0, 0, 50);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    ct = CATransform3DMakeTranslation(50, 0, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 0, 1, 0);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    ct = CATransform3DMakeTranslation(0, -50, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 1, 0, 0);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    ct = CATransform3DMakeTranslation(0, 50, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 1, 0, 0);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    ct = CATransform3DMakeTranslation(-50, 0, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 0, 1, 0);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    ct = CATransform3DMakeTranslation(0, 0, -50);
+    ct = CATransform3DRotate(ct, M_PI, 0, 1, 0);
+    [cubeLayer addSublayer:[self faceWithTransform:ct]];
+
+    CGSize contentSize = self.contentView.bounds.size;
+    cubeLayer.position = CGPointMake(contentSize.width / 2, contentSize.height / 2);
+
+    cubeLayer.transform = transfrom;
+    return cubeLayer;
+}
+
+- (CALayer *)faceWithTransform:(CATransform3D)transform {
+    // 创建立方体 面
+    CALayer *face = [CALayer layer];
+    face.frame = CGRectMake(-50, -50, 100, 100);
+
+    // 生成随机颜色
+    CGFloat red = (rand() / (double)INT_MAX);
+    CGFloat green = (rand() / (double)INT_MAX);
+    CGFloat blue = (rand() / (double)INT_MAX);
+    face.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    face.transform = transform;
+    return face;
 }
 
 /*
